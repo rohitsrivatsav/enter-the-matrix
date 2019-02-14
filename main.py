5c47981784b1488bab2a99c508e2a9f891a5e612
Rohit T <rohit.tummalapalli@oxc02y34aujgh6.mckesson.com>
2019-02-14 08:44:12 -0600

    Added matric code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from matrix import Matrix
+from vector import Vector
+from functools import reduce
 
-m1 = Matrix.random(3, 4)
+# m1 = Matrix.random(3, 4)
 
 # 1 cry
 # 2 goto 1
@@ -15,3 +17,47 @@ m1 = Matrix.random(3, 4)
 # and add them together
 
 # multiply a matrix times a vector
+
+with open("matrix.txt") as f:
+    vectors = []
+    matrix_list = []
+    for line in f:
+        if(len(line) > 1):
+            v = Vector(*tuple(map(int, line.split(' '))))
+            vectors.append(v)
+            pass
+        else:
+            m = Matrix(*vectors)
+            matrix_list.append(m)
+            vectors = []
+            pass
+
+    # Seperated lists of 2x4 and 4x2
+    matrix_dict = {}
+    for m in matrix_list:
+        if(m.size() in matrix_dict):
+            matrix_dict[m.size()].append(m)
+        else:
+            matrix_dict[m.size()] = [m]
+
+print(matrix_dict['2x4'][0] + matrix_dict['2x4'][1])
+print(Matrix.random(2, 3))
+m1 = matrix_dict['2x4'][0]
+m2 = matrix_dict['4x2'][0]
+# Scale Matrix
+m1.scale(3)
+# Multiply different size matrix
+print(m1.mul(m2))
+# print(matrix_dict)
+# print(matrix)
+# print(matrix[2])
+# v = tuple(map(int, rows))
+# print(v)
+# print(len(rows))
+# while len(rows) > 1:
+#     print(rows)
+
+# print(v)
+mat5 = [Matrix.random(3, 3) for _ in range(5)]
+mat_empty = Matrix(*[Vector(0, 0, 0), Vector(0, 0, 0), Vector(0, 0, 0)])
+print(reduce(lambda total, mat: total + mat, mat5, mat_empty))
--- matrix.py
+++ matrix.py
@@ -6,8 +6,72 @@
 # create a random mxn matrix
 # with values between 0 and 1
 
+from vector import Vector
+import random as r
+
 
 class Matrix:
-    @staticmethod
-    def random(params):
+
+    def __init__(self, *vec):
+        self.matrix = vec
+        self.matrix_rows = len(vec)
+        self.matrix_cols = len(vec[0])
         pass
+
+    @staticmethod
+    def random(row, col):
+        v1 = []  # list of numbers
+        vec = []
+        for _ in range(row):
+            for _ in range(col):
+                v1.append(r.random())
+            vec.append(Vector(*v1))
+            v1 = []
+        return Matrix(*vec)
+
+    def size(self):
+        return "{}x{}".format(self.matrix_rows, self.matrix_cols)
+
+    def __str__(self):
+        msg1 = "This is a {} X {} matrix".format(self.matrix_rows, self.matrix_cols)
+        msg2 = [x.nums for x in self.matrix]
+        return "{} \n {}".format(msg1, msg2)
+
+    def __add__(self, mat):
+        return Matrix(*[c1 + c2 for c1, c2 in zip(self.matrix, mat.matrix)])
+
+    def scale(self, scalar):
+        return Matrix(*[n.scale(scalar) for n in self.matrix])
+
+        # v1 = Vector(1, 2, 3)
+        # v2 = Vector(3, 4, 5)
+        # m1 = Matrix(v1, v2)
+        # print(m1)
+        # print(matrix)
+        # m1 = Matrix(matrix)
+        # print(m1)
+        # for x in m1.matrix[0]:
+        #     print(len(x))
+        # for x in m1.matrix[0]:
+        #     print(x)
+    def transpose(self):
+        v = []
+        m = []
+        for col in range(self.matrix_cols):
+            for row in self.matrix:
+                v.append(row.nums[col])
+            m.append(Vector(*v))
+            v = []
+        return Matrix(*m)
+
+    def mul(self, other):
+        t_other = other.transpose()
+        v = []
+        m = []
+        for row in self.matrix:
+            for col in t_other.matrix:
+                v.append(row.dot(col))
+            vec = Vector(*v)
+            m.append(vec)
+            v = []
+        return Matrix(*m)
