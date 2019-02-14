307fee16c449fbd6aa678796a4b04eb1e6ed1f28
Chyld Medford <chyld.medford@gmail.com>
2019-02-13 18:09:40 -0800

    fin

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
+from collections import defaultdict
+from functools import reduce
+
 from matrix import Matrix
 
-m1 = Matrix.random(3, 4)
+from vector import Vector
 
-# 1 cry
-# 2 goto 1
-# read in the matrix.txt
-# filter the 4x2 and 2x4 matrices into
-# different lists
+# load matrices from file
+vectors = []
+matrices = []
+with open('matrix.txt') as f:
+    for line in f:
+        line = line.strip()
+        if not line:
+            matrix = Matrix(*vectors)
+            matrices.append(matrix)
+            vectors = []
+        else:
+            vector = Vector(*map(float, line.split(' ')))
+            vectors.append(vector)
+    matrix = Matrix(*vectors)
+    matrices.append(matrix)
 
-# add any 2 compatible matrices
-# multiply any 2 compatible matrices
-# scale any matrix
-# create a list of 5 3x3 matrices of random values
-# and add them together
+# print out all matrices
+for i, m in enumerate(matrices):
+    print(i, 'matrix:', m.shape, '\n', m, '\n', '-' * 25)
 
-# multiply a matrix times a vector
+# filter matrices
+d = defaultdict(list)
+[d[m.shape].append(m) for m in matrices]
+print('filtered:', d)
+
+# create 5 3x3 matrices and add together
+zero = Vector(0, 0, 0)
+final = reduce(lambda acc, mat: acc + mat,
+               [Matrix.random(3, 3) for i in range(5)],
+               Matrix(zero, zero, zero))
+print('final:', final)
+
+# find the dot product of two matrices
+dotted = d[(2, 4)][0].dot(d[(4, 2)][0])
+print('dotted:', '\n', dotted)
--- matrix.py
+++ matrix.py
@@ -1,13 +1,41 @@
-# create a matrix
-# any dimension
-# multiply 2 matrices
-# add 2 matrices
-# scale matrix
-# create a random mxn matrix
-# with values between 0 and 1
+from random import random
+
+from vector import Vector
 
 
 class Matrix:
     @staticmethod
-    def random(params):
-        pass
+    def random(rows, cols):
+        randrows = [[random() for c in range(cols)] for r in range(rows)]
+        return Matrix(*[Vector(*row) for row in randrows])
+
+    def __init__(self, *vectors):
+        self.vectors = vectors
+        self.shape = (len(vectors), len(vectors[0].nums))
+
+    def __str__(self):
+        nl = '\n  '
+        return f"[{nl.join([str(v) for v in self.vectors])}]"
+
+    def __add__(self, other):
+        if self.shape != other.shape:
+            return
+        return Matrix(*[self.vectors[row] + other.vectors[row] for row in range(self.shape[0])])
+
+    def scale(self, scalar):
+        return Matrix(*[v.scale(scalar) for v in self.vectors])
+
+    def dot(self, other):
+        if self.shape != other.shape[::-1]:
+            return
+        column_vectors = [Vector(*col) for col in zip(*[v.nums for v in other.vectors])]
+        transposed = Matrix(*column_vectors)
+        vectors = []
+        for row in self.vectors:
+            nums = []
+            for col in transposed.vectors:
+                scalar = row.dot(col)
+                nums.append(scalar)
+            vector = Vector(*nums)
+            vectors.append(vector)
+        return Matrix(*vectors)
